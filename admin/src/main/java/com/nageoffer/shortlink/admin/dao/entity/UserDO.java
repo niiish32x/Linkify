c7809f5111b1c9e5912fc645ea4eafa851120fa1
Niii-sh <1159417019@qq.com>
2023-11-14 19:50:46 +0800

    日期自动填充字段的配置

--- admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/UserDO.java
@@ -1,9 +1,7 @@
 package com.nageoffer.shortlink.admin.dao.entity;
 
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -57,16 +55,19 @@ public class UserDO implements Serializable {
     /**
      * 创建时间
      */
+    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
     /**
      * 修改时间
      */
+    @TableField(fill = FieldFill.INSERT)
     private Date updateTime;
 
     /**
      * 删除标识 0：未删除 1：已删除
      */
+    @TableField(fill = FieldFill.INSERT)
     private Integer delFlag;
 
     public UserDO() {}
