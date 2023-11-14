d4fa1d1ba01384bb041ae2597a26c6f03a86d43a
Niii-sh <1159417019@qq.com>
2023-11-14 19:52:59 +0800

    添加用户注册功能register 使用布隆过滤器判断用户名是否重复 此外在布隆过滤器失效的场景 对Mysql 相应字段设置索引 在源头上防止username重复 最后再使用使用分布式锁完善注册功能 防止海量请求访问用户注册功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -2,6 +2,7 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
+import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import org.springframework.stereotype.Component;
 
@@ -19,8 +20,14 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 检查用户是否存在
-     * @param username
+     * @param username 用户名
      * @return 用户名存在返回True 不存在返回 False
      */
     Boolean hasUsername(String username);
+
+    /**
+     * 注销用户
+     * @param requestParam 注册用户请求参数
+     */
+    void register(UserRegisterReqDTO requestParam);
 }
--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
@@ -7,18 +7,35 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.nageoffer.shortlink.admin.common.convention.exception.ClientException;
 import com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum;
+import com.nageoffer.shortlink.admin.config.RBloomFilterConfiguration;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
 import com.nageoffer.shortlink.admin.dao.mapper.UserMapper;
+import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 //import org.springframework.beans.BeanUtils;
+import lombok.RequiredArgsConstructor;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
+
+import static com.nageoffer.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
+import static com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NAME_EXIST;
+import static com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SAVE_ERROR;
+
 
 /*
 *   用户接口实现层
 * */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+    // 布隆过滤器
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    // 分布式锁
+    private final RedissonClient redissonClient;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -37,9 +54,40 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     @Override
     public Boolean hasUsername(String username) {
-        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class).eq(UserDO::getUsername,username);
-        UserDO userDO = baseMapper.selectOne(queryWrapper);
-        return userDO == null;
+        // 直接使用布隆过滤器 判断用户名是否村子啊
+        return userRegisterCachePenetrationBloomFilter.contains(username);
+    }
+
+    @Override
+    public void register(UserRegisterReqDTO requestParam) {
+        // 没有返回 false
+        // !hasUsername 表示用户名已经存在 不能再注册相同用户名 所以抛出异常
+        if (!hasUsername(requestParam.getUsername())){
+            throw new ClientException(USER_NAME_EXIST);
+        }
+
+        // 如果不加 username 就成全局锁了
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
+
+        try {
+            if (lock.tryLock()){
+                // 如果成功拿到锁 则进行用户注册
+                int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if(inserted < 1){
+                    throw new ClientException(USER_SAVE_ERROR);
+                }
+
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(USER_NAME_EXIST);
+        }finally {
+            // 最后释放锁
+            lock.unlock();
+        }
+
+
+
     }
 
 }
