429abfa55985ed24450923c515a8568dbf1e13f8
Niii-sh <1159417019@qq.com>
2023-11-14 19:50:26 +0800

    Redis 锁相关的常量名 配置

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,10 @@
+package com.nageoffer.shortlink.admin.common.constant;
+
+/**
+ * 短链接项目
+ * Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+    // 冒号: 用于层次分离
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:user-register:";
+}
