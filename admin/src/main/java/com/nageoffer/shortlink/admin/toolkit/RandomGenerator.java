0e4f6a05fe0abac90d66a718c1bec1486622b84c
Niii-sh <1159417019@qq.com>
2023-11-16 19:33:43 +0800

    随机生成6位数的分组ID

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/toolkit/RandomGenerator.java
@@ -0,0 +1,28 @@
+package com.nageoffer.shortlink.admin.toolkit;
+
+/**
+ *  分组ID 随机生成器
+ */
+import java.security.SecureRandom;
+
+public final class RandomGenerator {
+    private static final String CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final int LENGTH = 6;
+
+    /**
+     * 生成随机分组ID 6位
+     * @return
+     */
+    public static String generateRandomString() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(LENGTH);
+
+        for (int i = 0; i < LENGTH; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+}
