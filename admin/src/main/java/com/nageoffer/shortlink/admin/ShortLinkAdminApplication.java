4d5ca635765d262bf6a6d8a916300b1fbbe1ea93
Niii-sh <1159417019@qq.com>
2023-11-20 20:03:26 +0800

    feature: 短链接中台启动类

--- admin/src/main/java/com/nageoffer/shortlink/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/ShortLinkAdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// 扫描接口 具体包路径 可以节省扫描流程 提升星能
+// 扫描接口 具体包路径 可以节省扫描流程 提升性能
 @MapperScan("com.nageoffer.shortlink.admin.dao.mapper")
 public class ShortLinkAdminApplication {
     public static void main(String[] args) {
