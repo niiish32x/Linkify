e97a703f2414da4666156e0972fe96845ce3c305
Niii-sh <1159417019@qq.com>
2023-11-20 19:54:09 +0800

    feature: 短链接管理启动类

---
+++ project/src/main/java/com/nageoffer/shortlink/project/ShortLinkApplication.java
@@ -0,0 +1,13 @@
+package com.nageoffer.shortlink.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.nageoffer.shortlink.project.dao.mapper")
+@SpringBootApplication
+public class ShortLinkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkApplication.class);
+    }
+}
