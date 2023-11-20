a1942a85d1bd165d207f15307d377004cc1dbb12
Niii-sh <1159417019@qq.com>
2023-11-20 20:03:09 +0800

    feature: 短连接后官 数据传输实体

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/req/ShortLinkGroupSortReqDTO.java
@@ -0,0 +1,19 @@
+package com.nageoffer.shortlink.admin.dto.req;
+
+import lombok.Data;
+
+/**
+ * 短链接分组排序参数
+ */
+@Data
+public class ShortLinkGroupSortReqDTO {
+    /**
+     * 分组ID
+     */
+    private String gid;
+
+    /**
+     * 排序字段
+     */
+    private Integer sortOrder;
+}
