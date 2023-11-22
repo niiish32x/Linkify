f03bfd65b92a108d9701a5c237cbf1bc62069fb6
Niii-sh <1159417019@qq.com>
2023-11-22 15:40:07 +0800

    feature: 添加分页功能

--- project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -1,10 +1,13 @@
 package com.nageoffer.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.nageoffer.shortlink.project.dao.entity.ShortLinkDO;
+import com.nageoffer.shortlink.project.dto.req.ShortLinkPageReqDTO;
 
 /**
  * 短链接持久层
  */
 public interface ShortLinkMapper extends BaseMapper<ShortLinkDO>{
+    IPage<ShortLinkDO> pageLink(ShortLinkPageReqDTO requestParam);
 }
